c3fe841d84b5c077dcf61a94d18b8b9306c9328b
shamsu557 <118239877+shamsu557@users.noreply.github.com>
2023-12-22 05:32:01 -0800

    Add files via upload

--- template.js
+++ template.js
@@ -29,6 +29,34 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 });
+// Get references to the search icon, search input, and search button elements
+const searchIcon = document.getElementById('searchIcon');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+// Add an event listener to the search icon
+searchIcon.addEventListener('click', function() {
+  // Toggle the 'd-none' class to show/hide the search input
+  searchInput.classList.toggle('d-none');
+  searchButton.classList.toggle('d-none');
+
+  // Focus on the search input when it's displayed
+  if (!searchInput.classList.contains('d-none')) {
+    searchInput.focus();
+  }
+});
+
+// Event listener for the search button (submit button)
+searchButton.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Retrieve the search query from the input field
+  const query = searchInput.value.trim();
+  
+  //search logic
+  
+});
+
  // Activate the carousel and set the interval
  $(document).ready(function(){
         $('.carousel').carousel({
@@ -70,3 +98,34 @@ function check() {
   }
 }
 check();
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.newsletter-form');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+      // Get the email input value
+      const emailInput = form.querySelector('.form-control').value;
+
+      // Validate the email format (simple validation)
+      if (!validateEmail(emailInput)) {
+          // Display error message or handle invalid email format
+          alert('Please enter a valid email address.');
+          return;
+      }
+
+      //  form submission or send data to your server
+      //  logic for form submission
+
+      // Show success message (temporarily an alert)
+      alert('Thank you for subscribing!');
+      // Optionally, you can reset the form after successful submission
+      form.reset();
+  });
+
+  // Function to validate email format
+  function validateEmail(email) {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
+  }
+});
